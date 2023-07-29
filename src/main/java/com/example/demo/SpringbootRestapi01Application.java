4ec86cfa739117226069521a5ce76726d805d2d4
Rakesh Kumar Behera <rakeshkumarbehera221@gmail.com>
2023-07-29 11:48:02 +0530

    first commit

---
+++ src/main/java/com/example/demo/SpringbootRestapi01Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestapi01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestapi01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/SpringbootRestapi01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestapi01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
